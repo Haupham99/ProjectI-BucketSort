5983687cb13902842a8b2e09807f17daf5aaf898
p29hieu <p29hieu@gmail.com>
2019-12-19 19:41:08 +0700

    change sdt hai ba trung

--- thongKeSdt.py
+++ thongKeSdt.py
@@ -1,4 +1,13 @@
-             
+
+def insertionSort(b): 
+    for i in range(1, len(b)): 
+        up = b[i] 
+        j = i - 1
+        while j >=0 and b[j] > up:  
+            b[j + 1] = b[j] 
+            j -= 1
+        b[j + 1] = up      
+    return b            
 def bucketSort(arr, num): 
     arr2 = [] 
     slot_num = num
