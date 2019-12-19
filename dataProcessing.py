3aa00cdee847aa068d77d8c3e5e71e1d77baab4f
Haupham99 <haupham99a@gmail.com>
2019-12-19 19:17:10 +0700

    upLastDataQuanHaiBaTrung

---
+++ dataProcessing.py
@@ -0,0 +1,41 @@
+import codecs
+import json
+
+f = codecs.open('quan-hai-ba-trung.txt', encoding='utf-8')
+
+#Dữ liệu cuối cùng
+dataDict = {}
+
+#Cho dữ liệu vào mảng
+arr = []
+k = 0
+
+#Dữ liệu thô
+data = f.read()
+
+f.close()
+
+arr = data.split(',')
+print(arr[0])
+
+i = 0
+for x in arr:
+	x = x[1:len(x)-1]
+	# print(x)
+	nameAndPhone = x.split('-')
+
+	if(len(nameAndPhone) > 2):
+		nameAndPhone[0] = nameAndPhone[len(nameAndPhone)-2]
+		nameAndPhone[1] = nameAndPhone[len(nameAndPhone)-1]
+	# print(nameAndPhone[0], nameAndPhone[1])
+	nameAndPhone[1] = nameAndPhone[1][7:len(nameAndPhone[1])]
+
+	nameAndPhone[1] = int(nameAndPhone[1].replace('.',''))
+	# if i < 20 :
+		# print(nameAndPhone[0], nameAndPhone[1])
+		# i = i+1
+	dataDict[nameAndPhone[0]] = nameAndPhone[1]
+
+print(dataDict)
+with open("sdt-Hai-Ba-Trung.json", 'w', encoding='utf-8') as f:
+	json.dump(dataDict, f)
